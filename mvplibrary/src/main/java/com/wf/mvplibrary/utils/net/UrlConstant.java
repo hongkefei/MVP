e3532723739371e78154458f715c1397952467f6
hongkefei <cjsswf@126.com>
2020-11-26 16:28:49 +0800

    添加baseurl

--- mvplibrary/src/main/java/com/wf/mvplibrary/utils/net/UrlConstant.java
+++ mvplibrary/src/main/java/com/wf/mvplibrary/utils/net/UrlConstant.java
@@ -5,6 +5,6 @@ package com.wf.mvplibrary.utils.net;
  * @time : 2020-11-12-14:38
  */
 public class UrlConstant {
-    public static String BASEURL = "";
+    public static String BASEURL = "https://cdplay.cn/api/";
 
 }
